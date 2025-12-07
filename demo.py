9c5c6cc084af609909b0b5149c44d94f11c02cb2
Hoai Nam Vo <duyminhstarlight@gmail.com>
2025-12-07 19:42:34 +0700

    1st commennt

---
+++ demo.py
@@ -0,0 +1,18 @@
+class arithmeticSequence:
+    def __init__(self, a1, n, d):
+        self.a1 = a1
+        self.n = n
+        self.d = d
+    
+    def nth_term(self):
+        return self.a1 + (self.n - 1) * self.d
+        
+    def sum_n_term(self):
+        total = 0
+        for i in range(self.n):
+            total = total + self.a1 + i * self.d 
+        return total
+        
+seq = arithmeticSequence(a1=2, d=3, n=5)
+print(seq.nth_term())
+print(seq.sum_n_term())
